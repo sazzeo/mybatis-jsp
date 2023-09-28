b74dbc47ba7946cefa3edecaf990f500f281ebde
jiyeong-dt <imjee02@gmail.com>
2023-09-28 21:31:18 +0900

    feat: mybatis 설정 추가

---
+++ src/main/java/com/minhee/springbootstudy/config/MybatisConfig.java
@@ -0,0 +1,35 @@
+package com.minhee.springbootstudy.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class MybatisConfig {
+
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+        sessionFactory.setTypeAliasesPackage("com.minhee.springbootstudy");
+
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setCallSettersOnNulls(false);
+        configuration.setJdbcTypeForNull(null);
+        sessionFactory.setConfiguration(configuration);
+
+        return sessionFactory.getObject();
+    }
+}
